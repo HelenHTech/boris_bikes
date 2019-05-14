2511fea6c5dc4b87144925d7618e8a6723878743
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-14 16:35:31 +0100

    Initialsize function, 20 bikes added, updated methods to accomadate array

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,21 +1,20 @@
 class DockingStation
-  attr_reader :docked_bike
+  attr_reader :docked_bikes
   def initialize
-    @docked_bike = 1
+    @docked_bikes = []
   end
   def release_bike
-    if @docked_bike == 1
-      @docked_bike -= 1
-      Bike.new      
-    else 
+    if @docked_bikes.length > 0
+      @docked_bikes.pop   
+    else
       raise "No bikes"
     end
   end
-  def dock_bike
-    if @docked_bike == 1
+  def dock_bike(bike)
+    if @docked_bikes.length >= 20
       raise "Dock contains bike"
     else
-      @docked_bike += 1
+      @docked_bikes << bike
     end
   end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -6,26 +6,24 @@ describe DockingStation do
   end
 
   it 'Is bike working' do
-    docking_station = DockingStation.new
-    bike = docking_station.release_bike
+    bike = Bike.new
     expect(bike).to respond_to(:working?)
   end
 
   it 'Can dock a bike' do
     docking_station = DockingStation.new
-    bike = docking_station.release_bike
     expect(docking_station).to respond_to(:dock_bike)
   end
   
   it 'Can tell if docking station contains a bike' do
     docking_station = DockingStation.new
-    docking_station.release_bike
     expect{docking_station.release_bike}.to raise_error("No bikes")
   end
 
   it 'Can tell if docking station is full' do
     docking_station = DockingStation.new
-    expect{docking_station.dock_bike}.to raise_error("Dock contains bike")
+    bike = Bike.new
+    expect{21.times{docking_station.dock_bike(bike)}}.to raise_error("Dock contains bike")
   end
 
 end
