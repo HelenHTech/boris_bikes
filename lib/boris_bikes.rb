49975fd193e1ee6cb14b97fd8748a292bfa015af
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-14 15:38:43 +0100

    11_using_instance_variables.md completed

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,5 +1,17 @@
 class DockingStation
+  attr_reader :docked_bike
+  def initialize
+    @docked_bike = 1
+  end
   def release_bike
+    if @docked_bike == 1
+      @docked_bike -= 1
+      Bike.new      
+    else 
+      "No bikes"
+    end
+  end
+  def dock_bike
 
   end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -2,15 +2,25 @@ require 'boris_bikes'
 
 describe DockingStation do
   it 'Docking station responds to release bike' do
-    docking_station = DockingStation.new
-    bike = docking_station.release_bike
-    expect(bike).respond_to?(:release_bike)
+    expect(DockingStation.new).to respond_to(:release_bike)
   end
 
-  it 'Docking station responds to release bike' do
+  it 'Is bike working' do
     docking_station = DockingStation.new
     bike = docking_station.release_bike
-    expect(bike).respond_to?(:working?)
+    expect(bike).to respond_to(:working?)
+  end
+
+  it 'Can dock a bike' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(docking_station).to respond_to(:dock_bike)
+  end
+  
+  it 'Can tell if docking station contains a bike' do
+    docking_station = DockingStation.new
+    docking_station.release_bike
+    expect(docking_station.release_bike).to eq("No bikes")
   end
 
 end
