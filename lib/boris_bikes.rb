fed83dd3ab8cfbbd8e3368e06543a67da0dd2def
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-18 19:25:52 +0100

    Step 18 - Dealing with broken bikes - Comments removed

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -12,12 +12,6 @@ class DockingStation
     return @docked_bikes.pop if @broken_bike
   end
 
-  # it 'Docking stations to not release broken bikes' do
-  #   bike.not_working?
-  #   docking_station.dock_bike(bike)
-  #   docking_station.release_bike
-  #   expect(docking_station.docked_bikes.length).to eq(1)
-
   def dock_bike(bike)
     raise "Dock contains bike" if full?
     @docked_bikes << bike
