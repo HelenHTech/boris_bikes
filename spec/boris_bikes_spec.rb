20f83a8e471b237e989a59efc81791481228341c
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-18 19:42:19 +0100

    Step 19 - Isolating Tests with Doubles - Added test doubles to instance of classes. Receiving failures

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -6,18 +6,22 @@ let(:docking_station) { DockingStation.new }
 let(:bike) { Bike.new }
 
   it 'Can dock a bike' do
+    bike = double(:bike)
     expect(docking_station).to respond_to(:dock_bike)
   end
   
   it 'Can tell if docking station contains a bike' do
+    bike = double(:bike)
     expect{docking_station.release_bike}.to raise_error("No bikes")
   end
 
   it 'Can tell if docking station is full' do
+    bike = double(:bike)
     expect{(DockingStation::DEFAULT_CAPACITY+1).times{docking_station.dock_bike(bike)}}.to raise_error("Dock contains bike")
   end
 
   it 'When no capacity is specified, default to value 20' do
+    bike = double(:bike)
     expect(docking_station.capacity).to eq(20)
   end
 end
@@ -28,12 +32,14 @@ let(:docking_station) { DockingStation.new }
 let(:bike) { Bike.new }
 
   it 'Docking stations to accept bikes, broken or not' do
+    bike = double(:bike)
     bike.not_working?
     docking_station.dock_bike(bike)
     expect(docking_station.docked_bikes.length).to eq(1)
   end
   
   it 'Docking stations to not release broken bikes' do
+    bike = double(:bike)
     bike.not_working?
     docking_station.dock_bike(bike)
     docking_station.release_bike
