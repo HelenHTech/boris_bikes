ece1ce54647cb7a3f490eeca817bdac3ebaec7e9
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-18 19:51:31 +0100

    Step 20 - Mocking Behaviour on Doubles Implemented

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -3,25 +3,21 @@ require 'boris_bikes'
 describe DockingStation do
 
 let(:docking_station) { DockingStation.new }
-let(:bike) { Bike.new }
+let(:bike) { double :bike }
 
   it 'Can dock a bike' do
-    bike = double(:bike)
     expect(docking_station).to respond_to(:dock_bike)
   end
   
   it 'Can tell if docking station contains a bike' do
-    bike = double(:bike)
     expect{docking_station.release_bike}.to raise_error("No bikes")
   end
 
   it 'Can tell if docking station is full' do
-    bike = double(:bike)
     expect{(DockingStation::DEFAULT_CAPACITY+1).times{docking_station.dock_bike(bike)}}.to raise_error("Dock contains bike")
   end
 
   it 'When no capacity is specified, default to value 20' do
-    bike = double(:bike)
     expect(docking_station.capacity).to eq(20)
   end
 end
@@ -29,17 +25,17 @@ end
 describe Bike do
 
 let(:docking_station) { DockingStation.new }
-let(:bike) { Bike.new }
+let(:bike) { double :bike }
 
   it 'Docking stations to accept bikes, broken or not' do
-    bike = double(:bike)
+    allow(bike).to receive(:not_working?).and_return(true)
     bike.not_working?
     docking_station.dock_bike(bike)
     expect(docking_station.docked_bikes.length).to eq(1)
   end
   
   it 'Docking stations to not release broken bikes' do
-    bike = double(:bike)
+    allow(bike).to receive(:not_working?).and_return(true)
     bike.not_working?
     docking_station.dock_bike(bike)
     docking_station.release_bike
