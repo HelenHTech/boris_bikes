2f0776bebe838730fe9581930a3c80f928dfdd6d
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-23 09:20:05 +0100

    Garage_spec and Garage Class completed

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,3 +1,17 @@
 class Garage
+attr_reader :bikes_in_van
+  def initialize
+    @bikes_in_van = []
+  end
+
+  def collect_bike_from_garage(bike)
+    @bikes_in_van.push(bike)
+  end
+
+  def distribute_bike_to_station(bike)
+    @bikes_in_van.each { |each_bike| docking_Station.receive(each_bike) }
+    @bikes_in_van = []
+    return "Bikes have successfully be distributed to the docking station"
+  end
 
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -4,15 +4,16 @@ describe Garage do
 
   let (:garage) { Garage.new }
   let (:bike) { double(:bike, :not_working? => true) }
-  let (:shiny_bike) { double(:bike, :not_working? => true) }
+  let(:docking_station) { double(:docking_station, :recieve => [bike]) }
 
+    it 'Vans to collect working bikes from garages' do
+      garage.collect_bike_from_garage(bike)
+      expect(garage.bikes_in_van).to include(bike)
+    end
 
-  it 'Vans to collect working bikes from garages' do
-
-  end
-
-  it 'Vans to distribute working bikes to docking stations' do
-
-  end
+    it 'Vans to distribute working bikes to docking stations' do
+      garage.distribute_bike_to_station(bike)
+      expect(garage.bikes_in_van).to_not include(bike)
+    end
   
 end
